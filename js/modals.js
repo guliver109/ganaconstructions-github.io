128ffa78a0cf20048e4edf042d8979ba2c4cb34e
Marko Gusic <markogusic@aol.com>
2018-12-08 15:04:44 -0800

    final review

--- js/modals.js
+++ js/modals.js
@@ -200,7 +200,7 @@ var Modal = (function() {
     })
     $(document).mouseup(function (e) {
       var container = $("#contactForm");
-      var closeContainer = $(".closeModal")
+      var closeContainer = $(".closeModal-btn")
   
       if (!container.is(e.target) // if the target of the click isn"t the container...
           && container.has(e.target).length === 0) // ... nor a descendant of the container
