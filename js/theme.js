d2d95caef8d0309edd6815fe5c101c98a784c532
Marko Gusic <markogusic@aol.com>
2018-12-08 16:10:20 -0800

    final review

--- js/theme.js
+++ js/theme.js
@@ -87,7 +87,7 @@
         }
     };
     
-    //* Stellar 
+    // * Stellar 
     $(function(){
         $.stellar({
             horizontalScrolling: false,
@@ -115,56 +115,6 @@
             });
         };
     };
-    //* Testimonial Carosel
-    function partnersCarosel(){
-        if ( $('.partners').length ){
-            $('.partners').owlCarousel({
-                loop:true,
-                items:5,
-                margin:110,
-                autoplay:true,
-                response:true,
-                responsive:{
-                    300:{
-                        items:1, 
-                        margin:0,
-                    },
-                    500:{
-                        items:3, 
-                    },
-                    700:{
-                        items:3, 
-                    },
-                    800:{
-                        items:4,
-                        margin:40,
-                    },
-                    1000:{
-                        items:5,
-                    },  
-                }
-            });
-        };
-    };
-    
-    //* waypoint JS
-    function ourSkrill(){
-         if ( $('.our_skill_inner').length ){
-             $(".our_skill_inner").each(function() {
-                $(this).waypoint(function() {
-                    var progressBar = $(".progress-bar");
-                    progressBar.each(function(indx){
-                        $(this).css("width", $(this).attr("aria-valuenow") + "%")
-                    })
-                }, 
-                {
-                    triggerOnce: true,
-                    offset: 'bottom-in-view'
-
-                });
-            });
-         }
-    };
     
      //* counterUp 2 JS
     function counterUp2(){
@@ -176,17 +126,7 @@
         } 
     }; 
     
-    //* Hide Loading Box (Preloader)
-     function preloader(){
-        if ( $('.preloader').length ){ 
-             $(window).load(function() {
-                $('.preloader').delay(500).fadeOut('slow');
-                $('body').delay(500).css({'overflow':'visible'});
-            });
-        } 
-    };
-
-    //scroll on top button
+    //Scroll on top button
 	var btn = $("#toTopButton");
 	
 	$(window).scroll(function() {
@@ -205,13 +145,10 @@
     
     /*Function Calls*/ 
     searchFrom ();
-    new WOW().init();
 	home_main_slider();
     testimonialsCarosel ();
     portfolio_isotope ();
-    counterUp ();  
-    partnersCarosel ();
-    ourSkrill ();
+    counterUp (); 
     counterUp2 ();
     mainNavbar ();
     preloader ();
